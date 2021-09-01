d3a0aa4dd6f3ab8dcf74a296e2f8cd7df7a7ce27
Kittinut Pramhan <tong.kittinut@riseaccel.com>
2021-09-02 01:02:26 +0700

    add police health

--- plugins/PoliceHealth.cs
+++ plugins/PoliceHealth.cs
@@ -6,26 +6,37 @@ using System.Linq;
 
 namespace Oxide.Plugins
 {
-    [Info("Better Health", "birthdates", "2.2.3")]
+    [Info("Police", "birthdates", "2.2.3")]
     [Description("Ability to customize the max health")]
     public class PoliceHealth : RustPlugin
     {
-
-  
-        private void OnPlayerRespawned(BasePlayer player)
+        private void Init()
         {
-            SetHealth(player);
+            permission.RegisterPermission("Police", this);
         }
 
-        private void SetHealth(BasePlayer player)
+          private void OnUserConnected(IPlayer player)
         {
-          if(player.HasPermission('police')){
-            Debug.Writeline('helloworld');
-          }
-          Debug.Writeline('helloworld2');
+            if (player.HasPermission("Police"))
+            {
+                player.maxHealth = 300f
+            }
+        }
+  
+        // private void OnPlayerRespawned(BasePlayer player)
+        // {
+        //     SetHealth(player);
+        // }
+
+        // private void SetHealth(BasePlayer player)
+        // {
+        //   if(player.HasPermission('police')){
+        //     Debug.Writeline('helloworld');
+        //   }
+        //   Debug.Writeline('helloworld2');
 
           
-        }
+        // }
 
     }
 }
